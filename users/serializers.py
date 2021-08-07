c225d0b5cc5ffb33c0ff4ac8d2543e2c06ff47a5
Nazar Kohut <nazarkohut0206@gmail.com>
2021-08-07 13:27:34 +0300

    Added basic registration(some fields aren't included);

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=100, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError("The username should contain only alphanumeric character")
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
