708b8ff31cdab28867f567c76b7a6af5cc6f2364
Nazar Kohut <nazarkohut0206@gmail.com>
2021-08-25 15:14:09 +0300

    Refactored ProblemSerializer in problems/serializers, removed Tags for problem into the same directory.

--- problems/serializers.py
+++ problems/serializers.py
@@ -2,17 +2,8 @@ from rest_framework import serializers
 from topics.models import Problem, Tag
 
 
-class TagsForProblemSerializer(serializers.ModelSerializer):
-    problems = serializers.ReadOnlyField(source='tags.problems')
-    id = serializers.ReadOnlyField(source='tags')
-
-    class Meta:
-        model = Tag
-        fields = '__all__'
-
-
 class ProblemSerializer(serializers.ModelSerializer):
-    tags = TagsForProblemSerializer(many=True)
+    tags = serializers.ListSerializer(child=serializers.CharField())
     sub_topics = serializers.ReadOnlyField(source='sub_topics.topic')
 
     class Meta:
