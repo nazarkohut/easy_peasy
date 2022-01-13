be803a1e03e9d08aac65b6125756bbde89719a54
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-01-13 20:00:18 +0200

    Added inserting rows to test_result table after test submission. Added a few fields to models in the test app. Structured code in SubmitTestView. Need to relate tests to users.

--- tests/models.py
+++ tests/models.py
@@ -28,6 +28,7 @@ class UserProfileTest(models.Model):
 class ProblemTest(models.Model):
     test = models.ForeignKey(Test, on_delete=models.SET_NULL, null=True)
     problem = models.ForeignKey(Problem, on_delete=models.SET_NULL, null=True)
+    cost = models.IntegerField()
 
     class Meta:
         db_table = "problem_test"
@@ -37,6 +38,7 @@ class TestResult(models.Model):  # results of previous tests
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name="previous_tests", null=True)
     mark = models.IntegerField()
     test_time = models.DateTimeField(auto_now_add=True)
+    problems_info = models.JSONField()
 
     class Meta:
         db_table = "test_result"
--- tests/views.py
+++ tests/views.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
 
-from django.db.models import F
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.utils import json
 
 from misc.converters import list_of_dicts_to_one_dict
-from tests.models import Test, ProblemTest
+from tests.models import Test, ProblemTest, TestResult
 from tests.serializers import AllTestListSerializer, TestSerializer, SubmitTestSerializer
 from topics.models import Problem
 
@@ -33,24 +32,47 @@ class SubmitTestView(generics.GenericAPIView):  # this one is incomplete
     serializer_class = SubmitTestSerializer
 
     def put(self, request, pk, **kwargs):
-        # queryset = ProblemTest.objects.select_related("problem").filter(test_id=pk)
+        cost_queryset = ProblemTest.objects.select_related("problem").filter(test_id=pk)
         queryset = Problem.objects.prefetch_related('problemtest_set').filter(problemtest__test_id=pk)
         user_answers = json.loads(request.body)
+        # converting query sets to convenient representation
+        costs = list_of_dicts_to_one_dict(cost_queryset.values(), key_param='problem_id', value_param='cost')
         data = list_of_dicts_to_one_dict(lst=queryset.values(), key_param='id', value_param='answer')
-        response = list()
-        for k in data.keys():
+        # data validation starts here
+        response, general_mark = self._check_test_answers(correct_answers=data, user_answers=user_answers, costs=costs)
+        self.insert_row_to_test_result(data=response, mark=general_mark)
+        return Response(response, status=200)
+
+    @staticmethod
+    def insert_row_to_test_result(data: list, mark: int):
+        test_result = TestResult(mark=mark, problems_info=data)  # need to relate user_profile
+        test_result.save(force_insert=True)
+
+    @staticmethod
+    def _check_test_answers(correct_answers: dict, costs: dict, user_answers: dict) -> [list, int]:
+        def invalid_answer(d: dict) -> dict:
+            d['message'] = 'Wrong Answer'
+            d['mark'] = '0'
+            return d
+
+        def valid_answer(d: dict, mark_counter: int) -> [dict, int]:
+            curr['message'] = 'Accepted'
+            curr['mark'] = str(costs[k])
+            mark_counter += costs[k]
+            return [d, mark_counter]
+
+        res = list()
+        general_mark = 0
+        for k in correct_answers.keys():
             curr = defaultdict(str)
             curr['id'] = k
             problem_id = str(k)  # from database =)
             if problem_id in user_answers:
-                if data[k] == user_answers[problem_id]:
-                    curr['message'] = 'Accepted'
+                if correct_answers[k] == user_answers[problem_id]:
+                    curr, general_mark = valid_answer(curr, general_mark)
                 else:
-                    curr['message'] = 'Wrong Answer'
+                    curr = invalid_answer(curr)
             else:
-                curr['message'] = 'Wrong Answer'
-            response.append(curr)
-
-        # response= {{'id': 1, 'message': 'Accepted', 'points': 1}, {'id': 3, 'message': Fail, 'points': 0}}
-        return Response(response)
-        # Select * from ProblemTest pt inner join Problem p on pt.problem_id=p.id where test_id=pk;
+                curr = invalid_answer(curr)
+            res.append(curr)
+        return [res, general_mark]
