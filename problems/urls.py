49e6416432a666c0c2f5e54650f30c30440bbf6c
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-01-22 23:04:23 +0200

    Now all urls have slash in the end.

--- problems/urls.py
+++ problems/urls.py
@@ -5,5 +5,5 @@ from problems.views import ProblemView, SubmitProblemView, AllProblemsListView
 urlpatterns = [
     path('all/', AllProblemsListView.as_view(), name='all_problems'),
     path('<int:pk>/', ProblemView.as_view(), name='problem'),
-    path('submit/<int:pk>', SubmitProblemView.as_view(), name='submit_problem')
+    path('submit/<int:pk>/', SubmitProblemView.as_view(), name='submit_problem')
 ]
--- tags/urls.py
+++ tags/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, re_path
+from django.urls import re_path
+
 from tags.views import ProblemsSortedByTagsListView
 
 urlpatterns = [
